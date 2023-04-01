844d956a6e1ae885344f8edf89cdc189510697b6
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2023-04-01 18:09:55 +0600

    Some programme move to another folder

--- 1.Structured Programming Language/vercity problrm/Pratice Mid/Important for exam/Fibonacci/Untitled3.c
+++ 1.Structured Programming Language/vercity problrm/Pratice Mid/Important for exam/Fibonacci/Untitled3.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 int  main(){
     int a,b=1,c=0,d=0;
-    printf("Please enter how many fibonakki number you want to show: ");
+    printf("N=");
     scanf("%d",&a);
-    printf("The fibonacci number is: ");
     for(int i=0;i<a;i++){
         printf("%d ",d);
         d=b+c;
